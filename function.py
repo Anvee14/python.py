52e99ca60369f41548693bf90f0b07e9222a8a7d
Anvee14 <anveesharm@gmail.com>
2021-01-13 16:27:40 +0530

    'swappingFile'

---
+++ function.py
@@ -0,0 +1,22 @@
+'''f = open('test.txt','a+')
+str1 = 'i am good\n'
+f.write(str1)'''
+# f.read()
+#f = open('test.txt','w')
+
+
+def sum(a, b):
+
+    print(a+b)
+    return a+b
+
+
+sum(2, 3)
+
+'''fl = f.readlines()
+for line in fl :
+    print(line)
+
+intro = 'my name is anvee , i am having a class'
+word = intro.split(',')
+print(word)'''
